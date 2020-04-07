a04251226ef3a5f417830603da115adc9fbcbf90
TDMiracle <584290367@qq.com>
2020-04-07 11:30:38 +0800

    重构发现

---
+++ app/src/main/java/com/example/myapplication/ui/find/DiscoveryFragment.java
@@ -0,0 +1,236 @@
+package com.example.myapplication.ui.find;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.myapplication.EverydayPhrase;
+import com.example.myapplication.R;
+import com.example.myapplication.ui.find.DeskMate.DeskmateActivity;
+import com.example.myapplication.util.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+//public class BannerFragment extends Fragment implements View.OnClickListener{
+    public class DiscoveryFragment extends Fragment{
+    private View mView;
+    private ViewPager mViewPaper;
+    private List<ImageView> images;
+    private List<View> dots;
+    private int currentItem;
+    //记录上一次点的位置
+    private int oldPosition = 0;
+    //存放图片的id
+    private int[] imageIds = new int[]{
+            R.drawable.banner_6,
+            R.drawable.banner_5,
+            R.drawable.banner_4,
+            R.drawable.banner_3,
+            R.drawable.banner_2
+    };
+    //存放图片的标题
+    private String[] titles = new String[]{
+            "No.1",
+            "No.2",
+            "No.3",
+            "No.4",
+            "No.5"
+    };
+    private TextView title;
+    private ViewPagerAdapter adapter;
+    private ScheduledExecutorService scheduledExecutorService;
+
+    private FindViewModel notificationsViewModel;
+    private View view;
+    String userId = "TD.Miracle";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(FindViewModel.class);
+        mView=inflater.inflate(R.layout.fragment_find, null);
+        setView();
+        view = mView;
+        //initView();
+        return view;
+    }
+    private void setView(){
+        mViewPaper = (ViewPager)mView.findViewById(R.id.vp);
+
+        //显示的图片
+        images = new ArrayList<ImageView>();
+        for(int i = 0; i < imageIds.length; i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(imageIds[i]);
+            images.add(imageView);
+        }
+        //显示的小点
+        dots = new ArrayList<View>();
+        dots.add(mView.findViewById(R.id.dot_0));
+        dots.add(mView.findViewById(R.id.dot_1));
+        dots.add(mView.findViewById(R.id.dot_2));
+        dots.add(mView.findViewById(R.id.dot_3));
+        dots.add(mView.findViewById(R.id.dot_4));
+
+        title = (TextView) mView.findViewById(R.id.title);
+        title.setText(titles[0]);
+
+        adapter = new ViewPagerAdapter();
+        mViewPaper.setAdapter(adapter);
+
+        mViewPaper.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+
+            @Override
+            public void onPageSelected(int position) {
+                title.setText(titles[position]);
+                dots.get(position).setBackgroundResource(R.drawable.wish);
+                dots.get(oldPosition).setBackgroundResource(R.drawable.wrong);
+
+                oldPosition = position;
+                currentItem = position;
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+        });
+    }
+
+//    private void initView() {
+//        LinearLayout find_deskmate = this.view.findViewById(R.id.find_deskmate);
+//        LinearLayout dailyWord = this.view.findViewById(R.id.word_daily);
+//        LinearLayout team = this.view.findViewById(R.id.team);
+//        LinearLayout around = this.view.findViewById(R.id.around);
+//        LinearLayout punch = this.view.findViewById(R.id.punch);
+//
+//        find_deskmate.setOnClickListener(this);
+//        dailyWord.setOnClickListener(this);
+//        team.setOnClickListener(this);
+//        around.setOnClickListener(this);
+//        punch.setOnClickListener(this);
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.find_deskmate:
+//                Utils.actionStart(getActivity(), DeskmateActivity.class, null, userId);
+//                break;
+//            case R.id.word_daily:
+//                Utils.actionStart(getActivity(), EverydayPhrase.class, null, userId);
+//                break;
+//            case R.id.around:
+//                //Utils.actionStart(getActivity(), );
+//                break;
+//            case R.id.punch:
+//                //
+//                break;
+//            case R.id.team:
+//                //
+//                break;
+//        }
+//    }
+
+    /*定义的适配器*/
+    public class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup view, int position, Object object) {
+            // TODO Auto-generated method stub
+//          super.destroyItem(container, position, object);
+//          view.removeView(view.getChildAt(position));
+//          view.removeViewAt(position);
+            view.removeView(images.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            // TODO Auto-generated method stub
+            view.addView(images.get(position));
+            return images.get(position);
+        }
+
+    }
+
+    /**
+     * 利用线程池定时执行动画轮播
+     */
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleWithFixedDelay(
+                new ViewPageTask(),
+                2,
+                2,
+                TimeUnit.SECONDS);
+    }
+
+
+    /**
+     * 图片轮播任务
+     * @author liuyazhuang
+     *
+     */
+    private class ViewPageTask implements Runnable{
+
+        @Override
+        public void run() {
+            currentItem = (currentItem + 1) % imageIds.length;
+            mHandler.sendEmptyMessage(0);
+        }
+    }
+
+    /**
+     * 接收子线程传递过来的数据
+     */
+    private Handler mHandler = new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            mViewPaper.setCurrentItem(currentItem);
+        };
+    };
+    @Override
+    public void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        if(scheduledExecutorService != null){
+            scheduledExecutorService.shutdown();
+            scheduledExecutorService = null;
+        }
+    }
+
+}
--- app/src/main/java/com/example/myapplication/ui/find/FindFragment.java
+++ app/src/main/java/com/example/myapplication/ui/find/FindFragment.java
@@ -50,7 +50,7 @@ public class FindFragment extends Fragment implements View.OnClickListener {
         View root = inflater.inflate(R.layout.fragment_find, container, false);
         view = root;
 
-        initBanner();
+
 
 	 initView();
         return root;
@@ -74,25 +74,7 @@ public class FindFragment extends Fragment implements View.OnClickListener {
     }
 
    
-    public void initBanner(){
-        banner = view.findViewById(R.id.banner);
-        ArrayList<String> imagelist = new ArrayList<>();
-        //添加图片
-        imagelist.add("drawable/banner_1.jpg");
-        imagelist.add("drawable/banner_2.jpg");
-        imagelist.add("drawable/banner_3.jpg");
 
-        banner.setImageLoader(new ImageLoader() {
-            @Override
-            public void displayImage(Context context, Object path, ImageView imageView) {
-                com.nostra13.universalimageloader.core.ImageLoader instance = com.nostra13.universalimageloader.core.ImageLoader.getInstance();
-                instance.init(ImageLoaderConfiguration.createDefault(getActivity()));
-                instance.displayImage((String) path,imageView);
-            }
-        });
-        banner.setImages(imagelist);
-        banner.start();
-    }
 
     @Override
     public void onClick(View v) {
@@ -114,76 +96,4 @@ public class FindFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
-    /**
-     * 轮播图组件初始化
-     */
- /*   private void initView() {
-        banner = (Banner) getActivity().findViewById(R.id.banner);
-        //放图片地址的集合
-        list_path = new ArrayList<>();
-        //放标题的集合
-        list_title = new ArrayList<>();
-
-        list_path.add(R.drawable.banner_1);
-        list_path.add(R.drawable.banner_2);
-        list_path.add(R.drawable.banner_3);
-        list_title.add("好好学习");
-        list_title.add("天天向上");
-        list_title.add("热爱生活");
-        //设置内置样式，共有六种可以点入方法内逐一体验使用。
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
-        //设置图片加载器，图片加载器在下方
-        banner.setImageLoader(new GlideLoader());
-        //设置图片网址或地址的集合
-        banner.setImages(list_path);
-        //设置轮播图的标题集合
-        banner.setBannerTitles(list_title);
-        //设置轮播间隔时间
-        banner.setDelayTime(3000);
-        //设置是否为自动轮播，默认是“是”。
-        banner.isAutoPlay(true);
-        //设置指示器的位置，小点点，左中右。
-        banner.setIndicatorGravity(BannerConfig.CENTER)
-                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
-                .setOnBannerListener(this)
-                //必须最后调用的方法，启动轮播图。
-                .start();
-    }
-
-    @Override*/
-    /**
-     * 处理按钮点击事件
-     */
-/*    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.backup:
-                Toast.makeText(getActivity(),"Back up",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.delete:
-                Toast.makeText(getActivity(),"Delete",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.settings:
-                Toast.makeText(getActivity(),"Setting",Toast.LENGTH_SHORT).show();
-                break;
-            default:break;
-        }
-        return true;
-    }
-
-
-    //自定义的图片加载器
-    private class GlideLoader extends ImageLoader implements ImageLoaderInterface {
-
-        @Override
-        public void displayImage(Context context, Object path, View imageView) {
-            Glide.with(context).load((Integer) path).into((ImageView) imageView);
-        }
-
-        //提供createImageView 方法，如果不用可以不重写这个方法，主要是方便自定义ImageView的创建
-        public ImageView createImageView(Context context) {
-            //使用fresco，需要创建它提供的ImageView，当然你也可以用自己自定义的具有图片加载功能的ImageView
-            return new ImageView(context);
-        }
-
-    }*/
 }
