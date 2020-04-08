d6f2ce6abaaf7393326ffc79a1bb5271027f8a6e
wgy30 <wu_robot@qq.com>
2020-04-08 19:29:14 +0800

    小组

--- app/src/main/java/com/example/myapplication/ui/find/TeamReceive.java
+++ app/src/main/java/com/example/myapplication/ui/find/TeamReceive.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myapplication.R;
+
 public class TeamReceive extends AppCompatActivity {
 
     @Override
